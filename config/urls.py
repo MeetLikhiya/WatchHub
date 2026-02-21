ec793c9a857b20003da8a63930a11a9a526beb1b
Meet <meetlikhiya88588@gmail.com>
2026-02-22 01:19:24 +0530

    Initial commit - Django WatchHub E-commerce Project

---
+++ config/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings # For serving media files during development
+from django.conf.urls.static import static #
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("watches.urls")),
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
